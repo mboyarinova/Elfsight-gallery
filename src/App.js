5a1fb730806f3009fb64bd32caf86298530f5b8c
mboyarinova <44038937+mboyarinova@users.noreply.github.com>
2020-10-20 11:17:32 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  /*fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json));*/
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
